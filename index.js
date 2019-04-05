546191341c7625e88b30c54fe2f31adf800eaefc
Muhammad Riza <riza.riza69@gmail.com>
2019-04-05 09:09:42 +0700

    Project JS Array

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log([1, 2, 3])
+console.log([0, 1, 2, 3])
+console.log(['Alpha', 100, true])
+
+const person = ['Alpha', 100, true]
+
+console.log(person)
+console.log(person[0])
+console.log(person[1])
+console.log(person[2])
+
+alert(person)
+alert(person[0])
+alert(person[1])
+alert(person[2])
+
+const name = ['adi', 3, 'maul']
+
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
